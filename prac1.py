bfe9a9b2bbdbd5017d6c3f7dddc7b516375f0806
Nitesh Idnani <idnaninitesh@gmail.com>
2016-02-18 23:39:17 +0530

    Basic Monopoly Board
    Created a basic monopoly board layout

--- prac1.py
+++ prac1.py
@@ -1,26 +1,79 @@
 import pygame
-
-
+ 
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+ 
+# This sets the WIDTH and HEIGHT of each grid location
+WIDTH = 40
+HEIGHT = 40
+ 
+# This sets the margin between each cell
+MARGIN = 1
+ 
+# Create a 2 dimensional array. A two dimensional
+# array is simply a list of lists.
+grid = []
+for row in range(10):
+    # Add an empty array that will hold each cell
+    # in this row
+    grid.append([])
+    for column in range(10):
+        grid[row].append(0)  # Append a cell
+ 
+# Set row 1, cell 5 to one. (Remember rows and
+# column numbers start at zero.)
+grid[1][5] = 1
+ 
+# Initialize pygame
 pygame.init()
-
-gameDisplay = pygame.display.set_mode((800,600))
-
-pygame.display.set_caption('Monopoly Game')
-
+ 
+# Set the HEIGHT and WIDTH of the screen
+WINDOW_SIZE = [640, 480]
+screen = pygame.display.set_mode(WINDOW_SIZE,pygame.RESIZABLE)
+ 
+# Set title of screen
+pygame.display.set_caption("Monopoly Game")
+ 
+# Loop until the user clicks the close button.
+done = False
+ 
+# Used to manage how fast the screen updates
 clock = pygame.time.Clock()
+ 
+# -------- Main Program Loop -----------
+while not done:
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            done = True  # Flag that we are done so we exit this loop
+        #GAME LOGIC
 
-crashed = False
-
-while not crashed:
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            crashed = True
-
-        print(event)
-
-    pygame.display.update()
+            
+    # Set the screen background
+    screen.fill(BLACK)
+ 
+    # Draw the grid
+    for row in range(10):
+        for column in range(10):
+            color = WHITE
+            if grid[row][column] == 1:
+                color = GREEN
+            if row == 0 or row == 9 or column == 0 or column == 9:
+                pygame.draw.rect(screen,
+                             color,
+                             [(MARGIN + WIDTH) * column + MARGIN,
+                              (MARGIN + HEIGHT) * row + MARGIN,
+                              WIDTH,
+                              HEIGHT])
+ 
+    # Limit to 60 frames per second
     clock.tick(60)
-
+ 
+    # Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+ 
+# Be IDLE friendly. If you forget this line, the program will 'hang'
+# on exit.
 pygame.quit()
-quit()
