1283586f1ac9a1edafde8e4519a07f10824d5ac0
Nitesh Idnani <idnaninitesh@gmail.com>
2016-02-20 03:38:29 +0530

    Added Player Info
    The create player info creates a section where the player can see the
    property owned by them and their respective balance amount

--- create_screen.py
+++ create_screen.py
@@ -29,8 +29,9 @@ OPTION_BOARD_SPACING = 10
 CARD_WIDTH = 99
 CARD_HEIGHT = 69
 CARD_MARGIN = 1
-INFO_WIDTH = 250
-INFO_HEIGHT = 770
+INFO_WIDTH = 294
+INFO_HEIGHT = 50
+INFO_MARGIN = 40
 GAME_OPTIONS_WIDTH = 1440
 GAME_OPTIONS_HEIGHT = 120
 OPTION_WIDTH = 130
@@ -729,20 +730,6 @@ def create_board():
 # creates the game play options(8) - roll dice, bulid, trade, sell, mortgage, unmortgage, rules, quit game
 #GAME_OPTIONS_WIDTH = 1440 width = 130
 #GAME_OPTIONS_HEIGHT = 120 y-coord = 810 height = 60
-"""
-
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GREEN = (0,255,0)
-BLUE = (0,0,255)
-ORANGE = (255,128,0)
-YELLOW = (255,255,0)
-BROWN = (165,42,42)
-LIGHT_BLUE = (198,226,255)
-PINK = (255,20,147)
-BOARD_GREEN = (230,255,230)
-RED = (255, 0, 0)
-"""
 
 def create_game_options():
 
@@ -860,7 +847,361 @@ def create_game_options():
 
 
 
+# --------------CREATE PLAYER INFO--------------
+# creates the player information box which shows the property owned by the player
+
+
+def create_player_info():
+
+    color = WHITE
+
+
+    # PLAYER 1
+    pygame.draw.rect(screen,
+                     color,
+                     (((BOARD_WIDTH + INFO_BOARD_SPACING/2),
+                       INFO_MARGIN),
+                      (INFO_WIDTH,
+                      INFO_HEIGHT)),2)
+
+    font = pygame.font.SysFont(CARD_TEXT_STYLE, 20)
+    screen.blit(font.render('PLAYER 1', True, WHITE), (1155, 55))
+    screen.blit(font.render('M 1500', True, WHITE), (1350, 55))
+
+
+    # PLAYER INFO
+    pygame.draw.rect(screen,
+                     color,
+                     (((BOARD_WIDTH + INFO_BOARD_SPACING/2),
+                       INFO_MARGIN + INFO_HEIGHT),
+                      (INFO_WIDTH,
+                      (INFO_HEIGHT + INFO_MARGIN) * 3 + 25)),2)
+
+    # KENTUCKY AVENUE
+    pygame.draw.rect(screen,
+                     RED,
+                     ((1180,
+                       100),
+                      (25,
+                       25)))
+
+
+    # INDIANA AVENUE
+    pygame.draw.rect(screen,
+                     RED,
+                     ((1230,
+                       100),
+                      (25,
+                       25)),2)
+
     
+    #   ILLINOIS AVENUE
+    pygame.draw.rect(screen,
+                     RED,
+                     ((1255,
+                       100),
+                      (25,
+                       25)),2)
+
+
+    #   B. & O. RAILROAD
+    pygame.draw.rect(screen,
+                     BLACK,
+                     ((1280,
+                       100),
+                      (25,
+                       25)),2)
+
+
+    #   ATLANTIC AVENUE
+    pygame.draw.rect(screen,
+                     YELLOW,
+                     ((1305,
+                       100),
+                      (25,
+                       25)),2)
+
+
+    #   VENTORA AVENUE
+    pygame.draw.rect(screen,
+                     YELLOW,
+                     ((1330,
+                       100),
+                      (25,
+                       25)))
+
+
+    #   WATER WORKS
+    pygame.draw.rect(screen,
+                     WHITE,
+                     ((1355,
+                       100),
+                      (25,
+                       25)),2)
+
+
+    #   MARVIN GARDENS
+    pygame.draw.rect(screen,
+                     YELLOW,
+                     ((1380,
+                       100),
+                      (25,
+                       25)),2)
+
+
+    #   PACIFIC AVENUE
+    pygame.draw.rect(screen,
+                     GREEN,
+                     ((1405,
+                       125),
+                      (25,
+                       25)),2)
+
+
+
+    #   CAROLINA AVENUE
+    pygame.draw.rect(screen,
+                     GREEN,
+                     ((1405,
+                       150),
+                      (25,
+                       25)),2)
+
+
+
+    #   PENNSYLVANIA AVENUE
+    pygame.draw.rect(screen,
+                     GREEN,
+                     ((1405,
+                       200),
+                      (25,
+                       25)))
+
+
+
+    #   SHORT LINE
+    pygame.draw.rect(screen,
+                     BLACK,
+                     ((1405,
+                       225),
+                      (25,
+                       25)))
+
+
+
+    #   PARK PLACE
+    pygame.draw.rect(screen,
+                     BLUE,
+                     ((1405,
+                       275),
+                      (25,
+                       25)),2)
+
+
+
+    #   BROADWALK
+    pygame.draw.rect(screen,
+                     BLUE,
+                     ((1405,
+                       325),
+                      (25,
+                       25)))
+
+
+
+    #   MEDITERRANEAN AVENUE
+    pygame.draw.rect(screen,
+                     BROWN,
+                     ((1380,
+                       350),
+                      (25,
+                       25)),2)
+
+
+    #   BALTIC AVENUE
+    pygame.draw.rect(screen,
+                     BROWN,
+                     ((1330,
+                       350),
+                      (25,
+                       25)))
+
+
+
+    #   READING RAILROAD
+    pygame.draw.rect(screen,
+                     BLACK,
+                     ((1280,
+                       350),
+                      (25,
+                       25)),2)
+
+
+
+    #   ORIENTAL AVENUE
+    pygame.draw.rect(screen,
+                     LIGHT_BLUE,
+                     ((1255,
+                       350),
+                      (25,
+                       25)),2)
+
+
+
+    #   VERMONT AVENUE
+    pygame.draw.rect(screen,
+                     LIGHT_BLUE,
+                     ((1205,
+                       350),
+                      (25,
+                       25)))
+
+
+
+    #   CONNECTICUT AVENUE
+    pygame.draw.rect(screen,
+                     LIGHT_BLUE,
+                     ((1180,
+                       350),
+                      (25,
+                       25)),2)
+
+
+
+    #   ST. CHARLES PLACE
+    pygame.draw.rect(screen,
+                     PINK,
+                     ((1155,
+                       325),
+                      (25,
+                       25)),2)
+
+
+
+    #   ELECTRIC COMPANY
+    pygame.draw.rect(screen,
+                     WHITE,
+                     ((1155,
+                       300),
+                      (25,
+                       25)))
+
+
+
+    #   STATES AVENUE
+    pygame.draw.rect(screen,
+                     PINK,
+                     ((1155,
+                       275),
+                      (25,
+                       25)),2)
+
+
+
+    #   VIRGINIA AVENUE
+    pygame.draw.rect(screen,
+                     PINK,
+                     ((1155,
+                       250),
+                      (25,
+                       25)))
+
+
+
+    #   PENNSYLVANIA RAILROAD
+    pygame.draw.rect(screen,
+                     BLACK,
+                     ((1155,
+                       225),
+                      (25,
+                       25)),2)
+
+
+
+    #   ST. JAMES PLACE
+    pygame.draw.rect(screen,
+                     ORANGE,
+                     ((1155,
+                       200),
+                      (25,
+                       25)),2)
+
+
+
+    #   TENNESSEE AVENUE
+    pygame.draw.rect(screen,
+                     ORANGE,
+                     ((1155,
+                       150),
+                      (25,
+                       25)))
+
+
+
+    #   NEW YORK AVENUE
+    pygame.draw.rect(screen,
+                     ORANGE,
+                     ((1155,
+                       125),
+                      (25,
+                       25)),2)
+
+
+
+
+
+
+    
+    # PLAYER 2
+    pygame.draw.rect(screen,
+                     color,
+                     (((BOARD_WIDTH + INFO_BOARD_SPACING/2),
+                       (INFO_MARGIN + INFO_HEIGHT) * 4 + 25),
+                      (INFO_WIDTH,
+                      INFO_HEIGHT)),2)
+
+    font = pygame.font.SysFont(CARD_TEXT_STYLE, 20)
+    screen.blit(font.render('PLAYER 2', True, WHITE), (1155, 400))
+    screen.blit(font.render('M 1500', True, WHITE), (1350, 400))
+
+
+    # PLAYER 3
+    pygame.draw.rect(screen,
+                     color,
+                     (((BOARD_WIDTH + INFO_BOARD_SPACING/2),
+                      (INFO_MARGIN + INFO_HEIGHT) * 5 - INFO_MARGIN + 25),
+                      (INFO_WIDTH,
+                      INFO_HEIGHT)),2)
+
+    font = pygame.font.SysFont(CARD_TEXT_STYLE, 20)
+    screen.blit(font.render('PLAYER 3', True, WHITE), (1155, 450))
+    screen.blit(font.render('M 1500', True, WHITE), (1350, 450))
+
+
+    # PLAYER 4
+    pygame.draw.rect(screen,
+                     color,
+                     (((BOARD_WIDTH + INFO_BOARD_SPACING/2),
+                      (INFO_MARGIN + INFO_HEIGHT) * 6 - INFO_MARGIN * 2 + 25),
+                      (INFO_WIDTH,
+                      INFO_HEIGHT)),2)
+
+    font = pygame.font.SysFont(CARD_TEXT_STYLE, 20)
+    screen.blit(font.render('PLAYER 4', True, WHITE), (1155, 500))
+    screen.blit(font.render('M 1500', True, WHITE), (1350, 500))
+
+
+    # BANK
+    pygame.draw.rect(screen,
+                     color,
+                     (((BOARD_WIDTH + INFO_BOARD_SPACING/2),
+                      (INFO_MARGIN + INFO_HEIGHT) * 7 - INFO_MARGIN * 3 + 25),
+                      (INFO_WIDTH,
+                      INFO_HEIGHT)),2)
+
+    font = pygame.font.SysFont(CARD_TEXT_STYLE, 20)
+    screen.blit(font.render('BANK', True, WHITE), (1155, 550))
+    screen.blit(font.render('M 6000', True, WHITE), (1350, 550))
     
     
 
@@ -898,6 +1239,8 @@ def run_game():
         pygame.draw.line(screen, color, (BOARD_WIDTH + INFO_BOARD_SPACING/2,0), (BOARD_WIDTH + INFO_BOARD_SPACING/2,BOARD_HEIGHT + OPTION_BOARD_SPACING/2))
 
         create_game_options()
+
+        create_player_info()
 
 
         clock.tick(60)
