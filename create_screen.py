bf4efee71298cfa45a789d397e6a8010620f038c
Nitesh Idnani <idnaninitesh@gmail.com>
2016-02-20 16:32:03 +0530

    Monopoly flow and pygame flow created
    Added how the game is going to proceed through events and updates.

--- create_screen.py
+++ create_screen.py
@@ -1215,6 +1215,7 @@ def run_game():
 
     clock = pygame.time.Clock()
 
+
     #--------------MAIN GAME LOOP------------------
     while not done:
         for event in pygame.event.get():
@@ -1229,18 +1230,6 @@ def run_game():
                 print("Click ", pos, "Grid coordinates : ", row, col)
 
 
-        create_board()
-
-
-        color = WHITE
-        
-        pygame.draw.line(screen, color, (0,BOARD_HEIGHT + OPTION_BOARD_SPACING/2), (SCREEN_WIDTH,BOARD_HEIGHT + OPTION_BOARD_SPACING/2))
-        
-        pygame.draw.line(screen, color, (BOARD_WIDTH + INFO_BOARD_SPACING/2,0), (BOARD_WIDTH + INFO_BOARD_SPACING/2,BOARD_HEIGHT + OPTION_BOARD_SPACING/2))
-
-        create_game_options()
-
-        create_player_info()
 
 
         clock.tick(60)
@@ -1268,9 +1257,89 @@ if __name__ == '__main__':
 
     pygame.display.set_caption("MONOPOLY")
 
+
+    create_board()
+
+        
+    pygame.draw.line(screen, WHITE, (0,BOARD_HEIGHT + OPTION_BOARD_SPACING/2), (SCREEN_WIDTH,BOARD_HEIGHT + OPTION_BOARD_SPACING/2))
+        
+    pygame.draw.line(screen, WHITE, (BOARD_WIDTH + INFO_BOARD_SPACING/2,0), (BOARD_WIDTH + INFO_BOARD_SPACING/2,BOARD_HEIGHT + OPTION_BOARD_SPACING/2))
+
+
+    create_game_options()
+
+
+    create_player_info()
+    
+
     run_game()
                                   
 
+"""
 
-    
+------------------MONOPOLY FLOW----------------
+
+
+# This is intentionally simplified pseudocode, but at the bottom-most level most
+#    simple games have a similar structure to this.
+
+#Construct your game objects
+players = [Player(args) for n in range(num_players)]
+cards = loadCards()   #or however you load your cards (files,database,etc)
+board = Board()
+
+#initialize your display context
+pygame_screen.init()  #or whatever is the correct syntax for Pygame
+
+# Main game loop
+while 1:
+    check_inputs()    #get key/mouse input inputs for this frame
+    handle_inputs()    #handle each input that has occurred during this frame
+    update_gamestate()    #update the state on the game (who's turn, etc)
+    update_display()    #update pygame window graphics (draw stuff, flip display)
+
+cleanup()
+"""
+
+
+"""
+
+----------------PYGAME FLOW-------------------
+
+import pygame
+
+def main():
+    #Set up the game and run the main game loop
+    pygame.init()      # Prepare the pygame module for use
+    surface_sz = 480   # Desired physical surface size, in pixels.
+
+    # Create surface of (width, height), and its window.
+    main_surface = pygame.display.set_mode((surface_sz, surface_sz))
+
+    # Set up some data to describe a small rectangle and its color
+    small_rect = (300, 200, 150, 90)
+    some_color = (255, 0, 0)        # A color is a mix of (Red, Green, Blue)
+
+    while True:
+        ev = pygame.event.poll()    # Look for any event
+        if ev.type == pygame.QUIT:  # Window close button clicked?
+            break                   #   ... leave game loop
+
+        # Update your game objects and data structures here...
+
+        # We draw everything from scratch on each frame.
+        # So first fill everything with the background color
+        main_surface.fill((0, 200, 255))
+
+        # Overpaint a smaller rectangle on the main surface
+        main_surface.fill(some_color, small_rect)
+
+        # Now the surface is ready, tell pygame to display it!
+        pygame.display.flip()
+
+    pygame.quit()     # Once we leave the loop, close the window.
+
+main()
+
+"""
     
