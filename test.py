0c9787043740f94c7a22e1dbeeae4fdc34885379
Nitesh Idnani <idnaninitesh@gmail.com>
2016-02-20 16:29:38 +0530

    Revert "Added files via upload"
    This reverts commit 1aadbc87dfd5dc7496a4b5f357e91c22d44be2ad.

--- test.py
+++ test.py
@@ -1,58 +1,77 @@
-'''
-    DiceSimulator.py
-    I don't know if I should say die or dice :()
-    Author: Alan Richmond, Python3.codes
-'''
-
-import pygame, random, time
-
-size = 100                      # Size of window/dice
-spsz = size//10                 # size of spots
-m = int(size/2)                 # mid-point of dice (or die?)
-l=t=int(size/4)                 # location of left and top spots
-r=b=size-l                      # location of right and bottom spots
-rolling = 12                    # times that dice rolls before stopping
-diecol = (255,255,127)          # die colour
-spotcol = (0,127,127)           # spot colour
-
-d = pygame.display.set_mode((1440, 900))
-d.fill((255,255,255))
-pygame.display.set_caption("Dice Simulator")
-
-for i in range(rolling):            # roll the die...
-    n=random.randint(1,6)                   # random number between 1 & 6
-    d.fill((255,255,255))                          # clear previous spots
-    if n % 2 == 1:
-        pygame.draw.circle(d,spotcol,(m,m),spsz)  # middle spot
-    if n==2 or n==3 or n==4 or n==5 or n==6:
-        pygame.draw.circle(d,spotcol,(l,b),spsz)  # left bottom
-        pygame.draw.circle(d,spotcol,(r,t),spsz)  # right top
-    if n==4 or n==5 or n==6:
-        pygame.draw.circle(d,spotcol,(l,t),spsz)  # left top
-        pygame.draw.circle(d,spotcol,(r,b),spsz)  # right bottom
-    if n==6:
-        pygame.draw.circle(d,spotcol,(m,b),spsz)  # middle bottom
-        pygame.draw.circle(d,spotcol,(m,t),spsz)  # middle top
-    
+"""
+ Simple graphics demo
+ 
+ Sample Python/Pygame Programs
+ Simpson College Computer Science
+ http://programarcadegames.com/
+ http://simpson.edu/computer-science/
+ 
+"""
+ 
+# Import a library of functions called 'pygame'
+import pygame
+ 
+# Initialize the game engine
+pygame.init()
+ 
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+ 
+PI = 3.141592653
+ 
+# Set the height and width of the screen
+size = (400, 500)
+screen = pygame.display.set_mode(size)
+ 
+pygame.display.set_caption("Rotate Text")
+ 
+# Loop until the user clicks the close button.
+done = False
+clock = pygame.time.Clock()
+ 
+text_rotate_degrees = 0
+ 
+# Loop as long as done == False
+while not done:
+ 
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            done = True  # Flag that we are done so we exit this loop
+ 
+    # All drawing code happens after the for loop and but
+    # inside the main while not done loop.
+ 
+    # Clear the screen and set the screen background
+    screen.fill(WHITE)
+ 
+    # Draw some borders
+    pygame.draw.line(screen, BLACK, [100,50], [200, 50])
+    pygame.draw.line(screen, BLACK, [100,50], [100, 150])
+ 
+    # Select the font to use, size, bold, italics
+    font = pygame.font.SysFont('Calibri', 25, True, False)
+ 
+    # Sideways text
+    text = font.render("Sideways text", True, BLACK)
+    text = pygame.transform.rotate(text, 270)
+    screen.blit(text, [0, 0])
+ 
+    # Upside Down text
+    text = font.render("Upside down text", True, BLACK)
+    text = pygame.transform.rotate(text, 180)
+    screen.blit(text, [30, 0])
+ 
+    # Go ahead and update the screen with what we've drawn.
+    # This MUST happen after all the other drawing commands.
     pygame.display.flip()
-    time.sleep(0.2)
-
-"""
-import random
-
-def dice_sim(die_size, rolls):
-
-    results = 0
-    dice_sum = 0
-
-    for i in range(0,rolls):
-        results = random.randint(1,die_size)
-        print("Die %d rolled %d." % (i+1,results))
-        dice_sum += results
-
-    print("Total of %d dice rolls is: %d" % (rolls,dice_sum))
-
-    return None
-
-dice_sim(6,2)
-"""
+ 
+    # This limits the while loop to a max of 60 times per second.
+    # Leave this out and we will use all CPU we can.
+    clock.tick(60)
+ 
+# Be IDLE friendly
+pygame.quit()
