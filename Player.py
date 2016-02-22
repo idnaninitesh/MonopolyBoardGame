9e5edeada040ff4d5d1529819d00e43b974e177e
Nitesh Idnani <idnaninitesh@gmail.com>
2016-02-22 23:15:49 +0530

    Implemented move player function
    Implemented the function to move the player on the board after each turn

--- Player.py
+++ Player.py
@@ -1,3 +1,9 @@
+import pygame
+from pygame.locals import *
+
+from variables import *
+
+
 class Player:
 
     
@@ -6,19 +12,7 @@ class Player:
         self.name = name
         self.color = color.upper()
         self.cur_balance = cur_balance
-        if self.color == "BLUE":
-            cur_position = (1020,720)
-        elif self.color == "RED":
-            cur_position = (1060,720)
-        elif self.color == "GREEN":
-            cur_position = (1020,750)
-        elif self.color == "YELLOW":
-            cur_position = (1060,750)
-        else:
-            cur_position = ()
-
-        self.cur_position = cur_position
-        self.next_position = self.cur_position
+        self.cur_position = ()
         self.property_owned = []
         self.property_mortgaged = []
         total_rails_owned = 0
@@ -28,12 +22,48 @@ class Player:
 
 
     #   move player
+
+    
+    #adding dice and pieces(blue,red,green,yellow
+    # x = x coordinate of the card
+    # y = y coordinate of the card
+    # blue position   = x + 20, y + 20
+    # red position    = x + 60, y + 20
+    # green position  = x + 20, y + 50
+    # yellow position = x + 60, y + 50
+
+
+
+    def move_player(self,screen,next_position):
+        if self.color == "BLUE":
+            pygame.draw.circle(screen,
+                               BLUE,
+                               next_position,
+                               PIECE_RADIUS)
+        elif self.color == "RED":
+            pygame.draw.circle(screen,
+                               RED,
+                               next_position,
+                               PIECE_RADIUS)
+        elif self.color == "GREEN":
+            pygame.draw.circle(screen,
+                               GREEN,
+                               next_position,
+                               PIECE_RADIUS)
+        elif self.color == "YELLOW":
+            pygame.draw.circle(screen,
+                               YELLOW,
+                               next_position,
+                               PIECE_RADIUS)
+
+
+            
+    
     #   update balance
     #   add property
     #   remove property
     #   mortgage property
     #   unmortgage property
     #   set jail card
-    #   update position
     #   remove player
     
