690925a9a2543bc3efc2d518eefd0d725f248dcb
Nitesh Idnani <idnaninitesh@gmail.com>
2016-02-24 19:05:23 +0530

    Defined rectangle for each card
    Added a create_card_rects function to create card object and a
    rectangle for each card

--- create_board.py
+++ create_board.py
@@ -5,6 +5,8 @@ from random import *
 from pygame.locals import *
 
 from variables import *
+from Card import *
+
 
 # --------------CREATE BOARD FUNCTION--------------
 # creates the board and all the cards inside it
@@ -881,3 +883,256 @@ def roll_dice(screen,no1=0,no2=0):
                                DICE_SPOT_RADIUS)
 
 
+
+# creating the cards
+# creating cards rectangles for handling events on them
+
+def create_card_rects():
+    
+
+    Cards = []
+    Rects = []
+
+    # card - id_no,name,color,cost,rent,mortgage_value,house_cost,hotel_cost,house_rent,hotel_rent,board_pos,info_pos
+    # rect - left,top,width,height
+
+    card = Card(0,"GO","",0,0,0,0,0,[],0,(1001,701),())
+    Cards.append(card)
+    rect = pygame.Rect((1001,701),(CARD_WIDTH,CARD_HEIGHT))
+    Rects.append(rect)
+    
+    card = Card(1,"MEDITARREANEAN AVENUE","BROWN",60,2,30,50,50,[10,30,90,160],250,(901,701),(1380,350))
+    Cards.append(card)
+    rect = pygame.Rect((901,701),(CARD_WIDTH,CARD_HEIGHT))
+    Rects.append(rect)
+
+    
+    card = Card(2,"COMMUNITY CHEST","",0,0,0,0,0,[],0,(801,701),())
+    Cards.append(card)
+    rect = pygame.Rect((801,701),(CARD_WIDTH,CARD_HEIGHT))
+    Rects.append(rect)
+
+
+    card = Card(3,"BALTIC AVENUE","BROWN",80,4,30,50,50,[20,60,80,320],450,(701,701),(1330,350))
+    Cards.append(card)
+    rect = pygame.Rect((701,701),(CARD_WIDTH,CARD_HEIGHT))
+    Rects.append(rect)
+
+
+    card = Card(4,"INCOME TAX","",0,0,0,0,0,[],0,(601,701),())
+    Cards.append(card)
+    rect = pygame.Rect((601,701),(CARD_WIDTH,CARD_HEIGHT))
+    Rects.append(rect)
+
+
+    card = Card(5,"READING RAILROAD","BLACK",200,25,100,0,0,[],0,(501,701),(1280,350))
+    Cards.append(card)
+    rect = pygame.Rect((501,701),(CARD_WIDTH,CARD_HEIGHT))
+    Rects.append(rect)
+
+
+    card = Card(6,"ORIENTAL AVENUE","LIGHT_BLUE",100,6,50,50,50,[30,90,270,400],450,(401,701),(1255,350))
+    Cards.append(card)
+    rect = pygame.Rect((401,701),(CARD_WIDTH,CARD_HEIGHT))
+    Rects.append(rect)
+
+   
+    card = Card(7,"CHANCE","",0,0,0,0,0,[],0,(301,701),())
+    Cards.append(card)
+    rect = pygame.Rect((301,701),(CARD_WIDTH,CARD_HEIGHT))
+    Rects.append(rect)
+
+   
+    card = Card(8,"VERMONT AVENUE","LIGHT_BLUE",100,6,50,50,50,[30,90,270,400],450,(201,701),(1205,350))
+    Cards.append(card)
+    rect = pygame.Rect((201,701),(CARD_WIDTH,CARD_HEIGHT))
+    Rects.append(rect)
+
+
+    card = Card(9,"CONNECTICUT AVENUE","LIGHT_BLUE",120,8,60,50,50,[40,100,300,450],600,(101,701),(1180,350))
+    Cards.append(card)
+    rect = pygame.Rect((101,701),(CARD_WIDTH,CARD_HEIGHT))
+    Rects.append(rect)
+
+
+    card = Card(10,"IN JAIL","",0,0,0,0,0,[],0,(1,701),())
+    Cards.append(card)
+    rect = pygame.Rect((1,701),(CARD_WIDTH,CARD_HEIGHT))
+    Rects.append(rect)
+
+
+    card = Card(11,"ST. CHARLES PLACE","PINK",140,10,70,100,100,[50,150,450,625],750,(1,631),(1155,325))
+    Cards.append(card)
+    rect = pygame.Rect((1,631),(CARD_WIDTH,CARD_HEIGHT))
+    Rects.append(rect)
+
+
+    card = Card(12,"ELECTIC COMPANY","WHITE",150,10,75,0,0,[],0,(1,561),(1155,300))
+    Cards.append(card)
+    rect = pygame.Rect((1,561),(CARD_WIDTH,CARD_HEIGHT))
+    Rects.append(rect)
+
+
+    card = Card(13,"STATES AVENUE","PINK",140,10,70,100,100,[50,150,450,625],750,(1,491),(1155,275))
+    Cards.append(card)
+    rect = pygame.Rect((1,491),(CARD_WIDTH,CARD_HEIGHT))
+    Rects.append(rect)
+
+
+    card = Card(14,"VIRGINIA AVENUE","PINK",160,12,80,100,100,[60,180,500,700],900,(1,421),(1155,250))
+    Cards.append(card)
+    rect = pygame.Rect((1,421),(CARD_WIDTH,CARD_HEIGHT))
+    Rects.append(rect)
+
+
+    card = Card(15,"PENNSYLVANIA RAILROAD","BLACK",200,25,100,0,0,[],0,(1,351),(1155,225))
+    Cards.append(card)
+    rect = pygame.Rect((1,351),(CARD_WIDTH,CARD_HEIGHT))
+    Rects.append(rect)
+
+
+    card = Card(16,"ST. JAMES PLACE","ORANGE",180,14,90,100,100,[70,200,550,750],950,(1,281),(1155,200))
+    Cards.append(card)
+    rect = pygame.Rect((1,281),(CARD_WIDTH,CARD_HEIGHT))
+    Rects.append(rect)
+
+
+    card = Card(17,"COMMUNITY CHEST","",0,0,0,0,0,[],0,(1,211),())
+    Cards.append(card)
+    rect = pygame.Rect((1,211),(CARD_WIDTH,CARD_HEIGHT))
+    Rects.append(rect)
+
+
+    card = Card(18,"TENNESSEE AVENUE","ORANGE",180,14,90,100,100,[70,200,550,750],950,(1,141),(1155,150))
+    Cards.append(card)
+    rect = pygame.Rect((1,141),(CARD_WIDTH,CARD_HEIGHT))
+    Rects.append(rect)
+
+
+    card = Card(19,"NEW YORK AVENUE","ORANGE",200,16,100,100,100,[80,220,600,800],1000,(1,71),(1155,125))
+    Cards.append(card)
+    rect = pygame.Rect((1,71),(CARD_WIDTH,CARD_HEIGHT))
+    Rects.append(rect)
+
+
+    card = Card(20,"FREE PARKING","",0,0,0,0,0,[],0,(1,1),())
+    Cards.append(card)
+    rect = pygame.Rect((1,1),(CARD_WIDTH,CARD_HEIGHT))
+    Rects.append(rect)
+
+
+    card = Card(21,"KENTUCKY AVENUE","RED",220,18,110,150,150,[90,250,700,875],1050,(101,1),(1180,100))
+    Cards.append(card)
+    rect = pygame.Rect((101,1),(CARD_WIDTH,CARD_HEIGHT))
+    Rects.append(rect)
+
+
+    card = Card(22,"CHANCE","",0,0,0,0,0,[],0,(201,1),())
+    Cards.append(card)
+    rect = pygame.Rect((201,1),(CARD_WIDTH,CARD_HEIGHT))
+    Rects.append(rect)
+
+
+    card = Card(23,"INDIANA AVENUE","RED",220,18,110,150,150,[90,250,700,875],1050,(301,1),(1230,100))
+    Cards.append(card)
+    rect = pygame.Rect((301,1),(CARD_WIDTH,CARD_HEIGHT))
+    Rects.append(rect)
+
+
+    card = Card(24,"ILLINOIS AVENUE","RED",240,20,120,150,150,[100,300,750,925],1100,(401,1),(1255,100))
+    Cards.append(card)
+    rect = pygame.Rect((401,1),(CARD_WIDTH,CARD_HEIGHT))
+    Rects.append(rect)
+
+
+    card = Card(25,"B. & O. RAILROAD","BLACK",200,25,100,0,0,[],0,(501,1),(1280,100))
+    Cards.append(card)
+    rect = pygame.Rect((501,1),(CARD_WIDTH,CARD_HEIGHT))
+    Rects.append(rect)
+
+
+    card = Card(26,"ATLANTIC AVENUE","YELLOW",260,22,130,150,150,[110,330,800,975],1150,(601,1),(1305,100))
+    Cards.append(card)
+    rect = pygame.Rect((601,1),(CARD_WIDTH,CARD_HEIGHT))
+    Rects.append(rect)
+
+
+    card = Card(27,"VENTORA AVENUE","YELLOW",260,22,130,150,150,[110,330,800,975],1150,(701,1),(1330,100))
+    Cards.append(card)
+    rect = pygame.Rect((701,1),(CARD_WIDTH,CARD_HEIGHT))
+    Rects.append(rect)
+
+
+    card = Card(28,"WATER WORKS","WHITE",150,10,75,0,0,[],0,(801,1),(1355,100))
+    Cards.append(card)
+    rect = pygame.Rect((801,1),(CARD_WIDTH,CARD_HEIGHT))
+    Rects.append(rect)
+
+
+    card = Card(29,"MARVIN GARDENS","YELLOW",280,24,140,150,150,[120,360,850,1025],1200,(901,1),(1380,100))
+    Cards.append(card)
+    rect = pygame.Rect((901,1),(CARD_WIDTH,CARD_HEIGHT))
+    Rects.append(rect)
+
+
+    card = Card(30,"GO TO JAIL","",0,0,0,0,0,[],0,(1001,1),())
+    Cards.append(card)
+    rect = pygame.Rect((1001,1),(CARD_WIDTH,CARD_HEIGHT))
+    Rects.append(rect)
+
+
+    card = Card(31,"PACIFIC AVENUE","GREEN",300,26,150,200,200,[130,390,900,1100],1275,(1001,71),(1405,125))
+    Cards.append(card)
+    rect = pygame.Rect((1001,71),(CARD_WIDTH,CARD_HEIGHT))
+    Rects.append(rect)
+
+
+    card = Card(32,"CAROLINA AVENUE","GREEN",300,26,150,200,200,[130,390,900,1100],1275,(1001,141),(1405,150))
+    Cards.append(card)
+    rect = pygame.Rect((1001,141),(CARD_WIDTH,CARD_HEIGHT))
+    Rects.append(rect)
+
+
+    card = Card(33,"COMMUNITY CHEST","",0,0,0,0,0,[],0,(1001,211),())
+    Cards.append(card)
+    rect = pygame.Rect((1001,211),(CARD_WIDTH,CARD_HEIGHT))
+    Rects.append(rect)
+
+
+    card = Card(34,"PENNSYLVANIA AVENUE","GREEN",320,28,160,200,200,[150,450,1000,1200],1400,(1001,281),(1405,200))
+    Cards.append(card)
+    rect = pygame.Rect((1001,281),(CARD_WIDTH,CARD_HEIGHT))
+    Rects.append(rect)
+
+
+    card = Card(35,"SHORT LANE","BLACK",200,25,100,0,0,[],0,(1001,351),(1405,225))
+    Cards.append(card)
+    rect = pygame.Rect((1001,351),(CARD_WIDTH,CARD_HEIGHT))
+    Rects.append(rect)
+
+
+    card = Card(36,"CHANCE","",0,0,0,0,0,[],0,(1001,421),())
+    Cards.append(card)
+    rect = pygame.Rect((1001,421),(CARD_WIDTH,CARD_HEIGHT))
+    Rects.append(rect)
+
+ 
+    card = Card(37,"PARK PLACE","BLUE",350,35,175,200,200,[175,500,1100,1300],1500,(1001,491),(1405,275))
+    Cards.append(card)
+    rect = pygame.Rect((1001,491),(CARD_WIDTH,CARD_HEIGHT))
+    Rects.append(rect)
+
+
+    card = Card(38,"LUXURY TAX","",0,0,0,0,0,[],0,(1001,561),())
+    Cards.append(card)
+    rect = pygame.Rect((1001,561),(CARD_WIDTH,CARD_HEIGHT))
+    Rects.append(rect)
+
+
+    card = Card(39,"BROADWALK","BLUE",400,50,200,200,200,[200,600,1400,1700],2000,(1001,631),(1405,325))
+    Cards.append(card)
+    rect = pygame.Rect((1001,631),(CARD_WIDTH,CARD_HEIGHT))
+    Rects.append(rect)
+
+    return (Cards,Rects)
+    
