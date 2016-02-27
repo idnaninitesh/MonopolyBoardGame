d8f3a5daddc929f47ca11586f8b9d667358b51fc
Nitesh Idnani <idnaninitesh@gmail.com>
2016-02-28 03:47:17 +0530

    Formatted the code

--- handle_dice_roll.py
+++ handle_dice_roll.py
@@ -15,95 +15,108 @@ from Player import *
 # roll dice and take action based on the new position of the current player
 
 def handle_dice_roll(screen,Rects,rect_index,Players,Cards,cur_player,Cards_Rects,Option_Rects,Info_Cards_Rects):
-
-
-    if True:
-        if True:                    # <----- Heights of laziness ;) ;)
     
-            # adding static part
-            
-            screen.fill(BACKGROUND_COLOR)
 
-            create_board(screen)
+    # adding static part
+    
+    screen.fill(BACKGROUND_COLOR)
 
-            create_game_options(screen)
+    create_board(screen)
 
-            #create_player_info(screen,Players,Cards,cur_player)
+    create_game_options(screen)
 
-
-            # rolling dice
-            
-            no1,no2 = roll_dice(screen)
-            steps = no1 + no2
- 
-            player = Players[cur_player]
-
-            initial_card = get_rect_pressed_index(player.cur_position,Cards_Rects)
-            final_card = (initial_card + steps)%40
+    create_player_info(screen,Players,Cards,cur_player)
 
 
-            # changing the current players' piece position
-           
-            final_pos = Cards[final_card].board_pos
-            next_position = ()
+    # rolling dice
+    no1,no2 = roll_dice(screen)
+    steps = no1 + no2
 
-            if player.color == "BLUE":
-                next_position = (final_pos[0] + 19,final_pos[1] + 19)
-            elif player.color == "RED":
-                next_position = (final_pos[0] + 59,final_pos[1] + 19)
-            elif player.color == "GREEN":
-                next_position = (final_pos[0] + 19,final_pos[1] + 49)
-            elif player.color == "YELLOW":
-                next_position = (final_pos[0] + 59,final_pos[1] + 49)
-            else:
-                next_position = ()
+    player = Players[cur_player]
 
-            player.cur_position = next_position
-            #player.move_player(screen,next_position)
-
-            # updating the position of all players' piece on the board
-            
-            for player in Players:
-                player.move_player(screen,player.cur_position)
+    initial_card = get_rect_pressed_index(player.cur_position,Cards_Rects)
+    final_card = (initial_card + steps)%40
 
 
-            # add the necessary actions to be taken due to dice roll
-            #final_card = update_game_dice(initial_card,final_card,no1,no2,Players,Cards,cur_player)
+    # changing the current players' piece position
+   
+    final_pos = Cards[final_card].board_pos
+    next_position = ()
 
-            # changing the position if there is some related instruction in chance or community card 
+    if player.color == "BLUE":
+        next_position = (final_pos[0] + 19,final_pos[1] + 19)
+    elif player.color == "RED":
+        next_position = (final_pos[0] + 59,final_pos[1] + 19)
+    elif player.color == "GREEN":
+        next_position = (final_pos[0] + 19,final_pos[1] + 49)
+    elif player.color == "YELLOW":
+        next_position = (final_pos[0] + 59,final_pos[1] + 49)
+    else:
+        next_position = ()
 
-            
-            """
+    player.cur_position = next_position
+    #player.move_player(screen,next_position)
 
-            final_pos = Cards[final_card].board_pos
-            next_position = ()
+    # updating the position of all players' piece on the board
+    
+    for temp in Players:
+        temp.move_player(screen,temp.cur_position)
 
-            if player.color == "BLUE":
-                next_position = (final_pos[0] + 19,final_pos[1] + 19)
-            elif player.color == "RED":
-                next_position = (final_pos[0] + 59,final_pos[1] + 19)
-            elif player.color == "GREEN":
-                next_position = (final_pos[0] + 19,final_pos[1] + 49)
-            elif player.color == "YELLOW":
-                next_position = (final_pos[0] + 59,final_pos[1] + 49)
-            else:
-                next_position = ()
 
-            player.cur_position = next_position
+    # add the necessary actions to be taken due to dice roll
+    final_card = update_game_dice(screen,initial_card,final_card,no1,no2,Players,Cards,cur_player)
 
-            screen.fill(BACKGROUND_COLOR)
+    # changing the position if there is some related instruction in chance or community card 
 
-            create_board(screen)
 
-            create_game_options(screen)
-            
-            for player in Players:
-                player.move_player(screen,player.cur_position)
+    final_pos = Cards[final_card].board_pos
+    next_position = ()
 
-            """
-            cur_player = (cur_player+1)%len(Players)
+    if player.color == "BLUE":
+        next_position = (final_pos[0] + 19,final_pos[1] + 19)
+    elif player.color == "RED":
+        next_position = (final_pos[0] + 59,final_pos[1] + 19)
+    elif player.color == "GREEN":
+        next_position = (final_pos[0] + 19,final_pos[1] + 49)
+    elif player.color == "YELLOW":
+        next_position = (final_pos[0] + 59,final_pos[1] + 49)
+    else:
+        next_position = ()
 
-            create_player_info(screen,Players,Cards,cur_player)
-            
+    player.cur_position = next_position
 
-            return True,cur_player
+    screen.fill(BACKGROUND_COLOR)
+
+    create_board(screen)
+
+    create_game_options(screen)
+
+    roll_dice(screen,no1,no2)
+    
+    for player in Players:
+        player.move_player(screen,player.cur_position)
+
+    create_player_info(screen,Players,Cards,cur_player)
+
+    display_end_turn_window(screen,Players,Cards,cur_player,Cards_Rects,Option_Rects,Info_Cards_Rects)
+
+    # change turn to next player
+
+    screen.fill(BACKGROUND_COLOR)
+
+    create_board(screen)
+
+    create_game_options(screen)
+
+    roll_dice(screen,no1,no2)
+    
+    for player in Players:
+        player.move_player(screen,player.cur_position)
+    
+    cur_player = (cur_player+1)%len(Players)
+
+    create_player_info(screen,Players,Cards,cur_player)
+
+    return cur_player
+    
+
