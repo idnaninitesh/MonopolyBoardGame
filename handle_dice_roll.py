b266014f0d27ece99d957e2a729f7d99a96dfabe
Nitesh Idnani <idnaninitesh@gmail.com>
2016-02-28 20:54:39 +0530

    Updated file

--- handle_dice_roll.py
+++ handle_dice_roll.py
@@ -3,18 +3,19 @@ import sys
 from pygame.locals import *
 
 from variables import *
+from Player import *
+from Card import *
 from create_board import *
 from create_game_options import *
 from create_player_info import *
 from handle_mouse_event import *
 from update_game_dice import *
-from Player import *
-
+from handle_game import *
 
 
 # roll dice and take action based on the new position of the current player
 
-def handle_dice_roll(screen,Rects,rect_index,Players,Cards,cur_player,Cards_Rects,Option_Rects,Info_Cards_Rects):
+def handle_dice_roll(screen,rect_index,Players,Cards,cur_player,Cards_Rects,Option_Rects,Info_Cards_Rects):
     
 
     # adding static part
@@ -29,6 +30,7 @@ def handle_dice_roll(screen,Rects,rect_index,Players,Cards,cur_player,Cards_Rect
 
 
     # rolling dice
+    
     no1,no2 = roll_dice(screen)
     steps = no1 + no2
 
@@ -85,13 +87,13 @@ def handle_dice_roll(screen,Rects,rect_index,Players,Cards,cur_player,Cards_Rect
 
     player.cur_position = next_position
 
+    # show the state after update
+
     screen.fill(BACKGROUND_COLOR)
 
     create_board(screen)
 
     create_game_options(screen)
-
-    roll_dice(screen,no1,no2)
     
     for player in Players:
         player.move_player(screen,player.cur_position)
@@ -108,7 +110,7 @@ def handle_dice_roll(screen,Rects,rect_index,Players,Cards,cur_player,Cards_Rect
 
     create_game_options(screen)
 
-    roll_dice(screen,no1,no2)
+    roll_dice(screen,4,2)
     
     for player in Players:
         player.move_player(screen,player.cur_position)
