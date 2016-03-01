d3e2a061f91eb9acb3f9d68ca8c03cf398c8750e
Nitesh Idnani <idnaninitesh@gmail.com>
2016-03-01 21:05:03 +0530

    Quit game handler
    Added code to handle quit game function

---
+++ handle_quit.py
@@ -0,0 +1,41 @@
+import pygame
+import sys
+from pygame.locals import *
+
+from variables import *
+from Player import *
+from Card import *
+from create_board import *
+from create_game_options import *
+from create_player_info import *
+from handle_mouse_event import *
+from update_game_dice import *
+from update_game_build import *
+from update_game_sell import *
+from update_game_mortgage import *
+from update_game_unmortgage import *
+
+def handle_quit(screen,Players,Cards,cur_player):
+
+    done = display_quit_window(screen)
+
+    if done == True:
+        pygame.quit()
+    else:
+
+        screen.fill(BACKGROUND_COLOR)
+
+        create_board(screen)
+
+        create_game_options(screen)
+
+        roll_dice(screen,4,2)
+        
+        for player in Players:
+            player.move_player(screen,player.cur_position)
+
+        create_player_info(screen,Players,Cards,cur_player)
+
+        return cur_player
+
+
