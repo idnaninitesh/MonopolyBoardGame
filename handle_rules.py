7e703bf87d4ef2682e88e4e11db4524bbd4e800c
Nitesh Idnani <idnaninitesh@gmail.com>
2016-03-05 16:05:19 +0530

    Game rules handler
    Added function to handle all steps involved in handling rules

---
+++ handle_rules.py
@@ -0,0 +1,40 @@
+import pygame
+import sys
+from pygame.locals import *
+
+from variables import *
+from Player import *
+from Card import *
+from create_board import *
+from create_game_options import *
+from create_player_info import *
+from handle_mouse_event import *
+from update_game_dice import *
+from update_game_build import *
+from update_game_sell import *
+from update_game_mortgage import *
+from update_game_unmortgage import *
+from display_windows import *
+
+def handle_rules(screen,Players,Cards,cur_player):
+
+    display_rules_window(screen)
+
+
+    # show the state after update
+    
+    screen.fill(BACKGROUND_COLOR)
+
+    create_board(screen)
+
+    create_game_options(screen)
+
+    roll_dice(screen,4,2)
+    
+    for player in Players:
+        player.move_player(screen,player.cur_position)
+
+    create_player_info(screen,Players,Cards,cur_player)
+
+    return cur_player
+
